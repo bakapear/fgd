e7e89ac440de70c796c07c4381a5d99cec499e4a
bakapear <proxybomb@gmail.com>
2019-11-17 21:37:31 +0100

    fixes

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ let fgd = require('./fgd/')
 let items = fgd('files/tf-puddy.fgd')
 
 // Apply base to all items and then remove base classes
-for (let i = 0; i < items.length; i++) {
-  let item = items[i]
-  if (item.parameters) {
-    let match = item.parameters.find(x => x.name === 'base')
-    if (match) {
-      match.values.forEach(value => {
-        let base = items.find(x => x.name === value)
-        let props = ['properties', 'flags', 'outputs', 'inputs']
-        for (let j = 0; j < props.length; j++) {
-          let prop = props[j]
-          if (base[prop]) {
-            if (!item[prop]) item[prop] = []
-            for (let k = 0; k < base[prop].length; k++) {
-              if (!item[prop].some(x => x === base[prop][k])) item[prop].push(base[prop][k])
-            }
+let applyBase = item => {
+  let match = item.parameters && item.parameters.find(x => x.name === 'base')
+  if (match) {
+    match.values.forEach(value => {
+      let base = items.find(x => x.name === value)
+      if (base.parameters && base.parameters.find(x => x.name === 'base')) base = applyBase(base)
+      let props = ['properties', 'flags', 'outputs', 'inputs']
+      props.forEach(prop => {
+        if (base[prop]) {
+          if (!item[prop]) item[prop] = []
+          for (let i = 0; i < base[prop].length; i++) {
+            if (!item[prop].some(x => x === base[prop][i])) item[prop].push(base[prop][i])
           }
         }
       })
-      item.parameters.splice(item.parameters.findIndex(x => x.name === 'base'), 1)
-      if (!item.parameters.length) delete item.parameters
-    }
+    })
+    if (!item.parameters.length) delete item.parameters
   }
+  return item
 }
-
-items = items.filter(x => x.type !== 'BaseClass')
+items = items.map(x => applyBase(x)).filter(x => x.type !== 'BaseClass').map(x => {
+  if (x.parameters) {
+    let index = x.parameters.findIndex(x => x.name === 'base')
+    if (index !== -1) x.parameters.splice(index, 1)
+    if (!x.parameters.length) delete x.parameters
+  }
+  return x
+})
 
 fs.writeFileSync('fgd.json', JSON.stringify(items, null, 2))
